28d9adfc35d90bfbb888ae358fab6c41c69364c0
Will Gale <willgaleproductions@gmail.com>
2023-05-20 23:02:37 +0100

    Added shcoreder reverb preset

--- include/utils/Constants.h
+++ include/utils/Constants.h
@@ -217,7 +217,7 @@ const String STEREO_DELAY_PRESET_TEXT = "Stereo Delay";
 const String FEEDFORWARD_COMB_PRESET_TEXT = "Feedforward Comb";
 const String FEEDBACK_COMB_PRESET_TEXT = "Feedback Comb";
 const String APF_PRESET_TEXT = "All Pass Filter";
-// const String SCHROEDER_PRESET_TEXT = "Schroeder Reverb";
+const String SCHROEDER_PRESET_TEXT = "Schroeder Reverb";
 // const String SPIN_SEMICONDUCTOR_PRESET_TEXT = "Spin Semiconductor Reverb";
 // const String FIGURE8_PRESET_TEXT = "Figure 8 Reverb";
 
--- src/ui/menus/MainMenu.cpp
+++ src/ui/menus/MainMenu.cpp
@@ -38,6 +38,7 @@ PopupMenu MainMenu::getMenuForIndex(int menuIndex, const String& menuName)
         menu.addItem(FEEDFORWARD_COMB_PRESET_TEXT, std::bind(&MainMenu::presetSelected, this, Presets::Type::FEEDFORWARD_COMB));
         menu.addItem(FEEDBACK_COMB_PRESET_TEXT, std::bind(&MainMenu::presetSelected, this, Presets::Type::FEEDBACK_COMB));
         menu.addItem(APF_PRESET_TEXT, std::bind(&MainMenu::presetSelected, this, Presets::Type::APF));
+        menu.addItem(SCHROEDER_PRESET_TEXT, std::bind(&MainMenu::presetSelected, this, Presets::Type::SCHROEDER_REVERB));
         // menu.addItem(FEEDBACK_COMB_PRESET_TEXT, std::bind(&MainMenu::presetSelected, this, PresetType::FeedforwardComb));
         // menu.addItem(APF_PRESET_TEXT, std::bind(&MainMenu::presetSelected, this, PresetType::APF));
         // menu.addItem(SCHROEDER_PRESET_TEXT, std::bind(&MainMenu::presetSelected, this, PresetType::SchroederReverb));
